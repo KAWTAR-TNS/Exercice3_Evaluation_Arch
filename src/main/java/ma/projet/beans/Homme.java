d075c1290303ede71565c83edd8e6b75e8d6aeeb
kawtar-tns <tanassakawtar@gmai.com>
2025-10-23 11:28:28 +0100

    .

---
+++ src/main/java/ma/projet/beans/Homme.java
@@ -0,0 +1,22 @@
+package ma.projet.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Homme extends Personne {
+    @OneToMany(mappedBy = "homme")
+    private List<Mariage> mariages;
+
+    // Getters et Setters
+
+
+    public List<Mariage> getMariages() {
+        return mariages;
+    }
+
+    public void setMariages(List<Mariage> mariages) {
+        this.mariages = mariages;
+    }
+}
